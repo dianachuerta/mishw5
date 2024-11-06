c36d903220ee09da44a36b1de6b6cd5a121a2983
dianachuerta <157341983+dianachuerta@users.noreply.github.com>
2024-11-06 13:37:08 -0600

    Create function5.php

---
+++ function5.php
@@ -0,0 +1,13 @@
+// Function to increment counter
+function add() {
+  let counter = 0;
+  counter += 1;
+  return counter;
+}
+
+// Call add() 3 times
+add();
+add();
+add();
+
+//The counter should now be 3. But it is 1.
