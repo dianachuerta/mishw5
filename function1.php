d7305bf9d2e8e46316f837c1f9cbf8ac7f7a914b
dianachuerta <157341983+dianachuerta@users.noreply.github.com>
2024-11-06 13:33:17 -0600

    Update function1.php

--- function1.php
+++ function1.php
@@ -1,7 +1,5 @@
-// Function is called, the return value will end up in x
-let x = myFunction(4, 3);
-
-function myFunction(a, b) {
-// Function returns the product of a and b
-  return a * b;
+function myFunction(x, y) {
+  if (y === undefined) {
+    y = 2;
+  }
 }
